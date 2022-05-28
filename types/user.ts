a425f2bf5ac86daf72738e7cf2eb7a257f7fa5dc
p1atdev <shu_teiei@outlook.jp>
2022-05-29 07:17:58 +0900

    fix: followsCount

--- types/user.ts
+++ types/user.ts
@@ -1,174 +1,174 @@
 export type User = {
-  /**
-   * プロフィール
-   */
-  profile: Profile;
+    /**
+     * プロフィール
+     */
+    profile: Profile
 
-  /**
-   * ユーザーアバター
-   */
-  avatar: Avatar;
+    /**
+     * ユーザーアバター
+     */
+    avatar: Avatar
 
-  /**
-   * バナー
-   */
-  banner: Banner;
-};
+    /**
+     * バナー
+     */
+    banner: Banner
+}
 
 /**
  * ユーザーアイコンに関する情報
  */
 export type Avatar = {
-  /**
-   * アイコン画像URL
-   */
-  url: string;
+    /**
+     * アイコン画像URL
+     */
+    url: string
 
-  /**
-   * デフォルトのアイコン画像か
-   */
-  isDefaultIcon: boolean;
+    /**
+     * デフォルトのアイコン画像か
+     */
+    isDefaultIcon: boolean
 
-  /**
-   * NFT?
-   */
-  isNFT: boolean;
-};
+    /**
+     * NFT?
+     */
+    isNFT: boolean
+}
 
 /**
  * バナーに関する情報
  */
 export type Banner = {
-  /**
-   * バナー画像のurl
-   */
-  url: string;
-};
+    /**
+     * バナー画像のurl
+     */
+    url: string
+}
 
 /**
  * ユーザーに関する情報
  */
 export type Profile = {
-  /**
-   * ユーザー名
-   */
-  userName: string;
+    /**
+     * ユーザー名
+     */
+    userName: string
 
-  /**
-   * `@`から始まるユーザーid
-   */
-  userId: string;
+    /**
+     * `@`から始まるユーザーid
+     */
+    userId: string
 
-  /**
-   * 数字だけのid
-   */
-  userRestId: string;
+    /**
+     * 数字だけのid
+     */
+    userRestId: string
 
-  /**
-   * bioとかの説明文章
-   */
-  description: string;
+    /**
+     * bioとかの説明文章
+     */
+    description: string
 
-  /**
-   * ユーザープロフィールのurl
-   */
-  profileUrl: string;
+    /**
+     * ユーザープロフィールのurl
+     */
+    profileUrl: string
 
-  /**
-   * webサイトリンクをつけてるときのurl
-   */
-  website: WebSite;
+    /**
+     * webサイトリンクをつけてるときのurl
+     */
+    website: WebSite
 
-  /**
-   * 認証済みか
-   */
-  isVerified: boolean;
+    /**
+     * 認証済みか
+     */
+    isVerified: boolean
 
-  /**
-   * フォロワーの数
-   */
-  followersCount: number;
+    /**
+     * フォロワーの数
+     */
+    followersCount: number
 
-  /**
-   * 早い時期からのフォロワーの数？
-   */
-  fastFollowersCount: number;
+    /**
+     * 早い時期からのフォロワーの数？
+     */
+    fastFollowersCount: number
 
-  /**
-   * 通常のフォロワーの数
-   */
-  normalFollowersCount: number;
+    /**
+     * 通常のフォロワーの数
+     */
+    normalFollowersCount: number
 
-  /**
-   * ファボの数
-   */
-  favouritesCount: number;
+    /**
+     * ファボの数
+     */
+    favouritesCount: number
 
-  /**
-   * 友達の数
-   */
-  friends_count: number;
+    /**
+     * フォロー
+     */
+    followsCount: number
 
-  /**
-   * 住所に設定されてる文字列
-   */
-  location: string;
+    /**
+     * 住所に設定されてる文字列
+     */
+    location: string
 
-  /**
-   * メディア数
-   */
-  mediaCount: number;
+    /**
+     * メディア数
+     */
+    mediaCount: number
 
-  /**
-   * リストに入れられている数
-   */
-  listedCount: number;
+    /**
+     * リストに入れられている数
+     */
+    listedCount: number
 
-  /**
-   * 翻訳者か
-   */
-  isTranslator: boolean;
+    /**
+     * 翻訳者か
+     */
+    isTranslator: boolean
 
-  /**
-   * 翻訳者のタイプ
-   */
-  translatorType: string;
+    /**
+     * 翻訳者のタイプ
+     */
+    translatorType: string
 
-  /**
-   * カスタムタイムラインを持っているか
-   */
-  hasCustomTimelines: boolean;
+    /**
+     * カスタムタイムラインを持っているか
+     */
+    hasCustomTimelines: boolean
 
-  /**
-   * 作成日時
-   */
-  createdAt: string;
+    /**
+     * 作成日時
+     */
+    createdAt: string
 
-  /**
-   * ピン留めしているツイートのid
-   * 基本的に一つで、設定されてなければ空
-   */
-  pinnedTweetIds: string[];
+    /**
+     * ピン留めしているツイートのid
+     * 基本的に一つで、設定されてなければ空
+     */
+    pinnedTweetIds: string[]
 
-  /**
-   * 鍵垢か
-   */
-  isProtected: boolean;
-};
+    /**
+     * 鍵垢か
+     */
+    isProtected: boolean
+}
 
 export type WebSite = {
-  /**
-   * 表示上のurl
-   */
-  displayUrl: string;
+    /**
+     * 表示上のurl
+     */
+    displayUrl: string
 
-  /**
-   * httpsとかがついたurl
-   */
-  expandedUrl: string;
+    /**
+     * httpsとかがついたurl
+     */
+    expandedUrl: string
 
-  /**
-   * 短縮url
-   */
-  shortenedUrl: string;
-};
+    /**
+     * 短縮url
+     */
+    shortenedUrl: string
+}
--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,77 +1,77 @@
-import { fetchUserData } from "./http.ts";
-import { Avatar, Banner, Profile, User, WebSite } from "../types/mod.ts";
+import { fetchUserData } from "./http.ts"
+import { Avatar, Banner, Profile, User, WebSite } from "../types/mod.ts"
 
 export const fetchUser = async (userName: string): Promise<User> => {
-  const res = await fetchUserData(userName);
+    const res = await fetchUserData(userName)
 
-  if (!res) {
-    throw new Error(`User ${userName} not found`);
-  }
-
-  const avatar: Avatar = {
-    url: res.result.legacy.profile_image_url_https.replace("_normal", ""), // delete _normal
-    isDefaultIcon: res.result.legacy.default_profile_image,
-    isNFT: res.result.has_nft_avatar,
-  };
-
-  const banner: Banner = {
-    url: res.result.legacy.profile_banner_url,
-  };
-
-  const webSite: WebSite = (() => {
-    try {
-      const urls = res.result.legacy.entities.url.urls;
-      if (urls.length === 0) {
-        return {
-          shortenedUrl: "",
-          expandedUrl: "",
-          displayUrl: "",
-        };
-      } else {
-        return {
-          shortenedUrl: urls[0].url,
-          expandedUrl: urls[0].expanded_url,
-          displayUrl: urls[0].display_url,
-        };
-      }
-    } catch {
-      return {
-        shortenedUrl: "",
-        expandedUrl: "",
-        displayUrl: "",
-      };
+    if (!res) {
+        throw new Error(`User ${userName} not found`)
     }
-  })();
 
-  const profile: Profile = {
-    userName: res.result.legacy.name,
-    userId: res.result.legacy.screen_name,
-    userRestId: res.result.rest_id,
-    description: res.result.legacy.description,
-    profileUrl: res.result.legacy.url,
-    website: webSite,
-    isVerified: res.result.legacy.verified,
-    followersCount: res.result.legacy.followers_count,
-    fastFollowersCount: res.result.legacy.fast_followers_count,
-    normalFollowersCount: res.result.legacy.normal_followers_count,
-    favouritesCount: res.result.legacy.favourites_count,
-    friends_count: res.result.legacy.friends_count,
-    location: res.result.legacy.location,
-    mediaCount: res.result.legacy.media_count,
-    listedCount: res.result.legacy.listed_count,
-    isTranslator: res.result.legacy.is_translator,
-    translatorType: res.result.legacy.translator_type,
-    hasCustomTimelines: res.result.legacy.has_custom_timelines,
-    createdAt: res.result.legacy.created_at,
-    pinnedTweetIds: res.result.legacy.pinned_tweet_ids_str,
-    isProtected: res.result.legacy.protected,
-  };
+    const avatar: Avatar = {
+        url: res.result.legacy.profile_image_url_https.replace("_normal", ""), // delete _normal
+        isDefaultIcon: res.result.legacy.default_profile_image,
+        isNFT: res.result.has_nft_avatar,
+    }
 
-  const user: User = {
-    avatar: avatar,
-    banner: banner,
-    profile: profile,
-  };
+    const banner: Banner = {
+        url: res.result.legacy.profile_banner_url,
+    }
 
-  return user;
-};
+    const webSite: WebSite = (() => {
+        try {
+            const urls = res.result.legacy.entities.url.urls
+            if (urls.length === 0) {
+                return {
+                    shortenedUrl: "",
+                    expandedUrl: "",
+                    displayUrl: "",
+                }
+            } else {
+                return {
+                    shortenedUrl: urls[0].url,
+                    expandedUrl: urls[0].expanded_url,
+                    displayUrl: urls[0].display_url,
+                }
+            }
+        } catch {
+            return {
+                shortenedUrl: "",
+                expandedUrl: "",
+                displayUrl: "",
+            }
+        }
+    })()
+
+    const profile: Profile = {
+        userName: res.result.legacy.name,
+        userId: res.result.legacy.screen_name,
+        userRestId: res.result.rest_id,
+        description: res.result.legacy.description,
+        profileUrl: res.result.legacy.url,
+        website: webSite,
+        isVerified: res.result.legacy.verified,
+        followersCount: res.result.legacy.followers_count,
+        fastFollowersCount: res.result.legacy.fast_followers_count,
+        normalFollowersCount: res.result.legacy.normal_followers_count,
+        favouritesCount: res.result.legacy.favourites_count,
+        followsCount: res.result.legacy.friends_count,
+        location: res.result.legacy.location,
+        mediaCount: res.result.legacy.media_count,
+        listedCount: res.result.legacy.listed_count,
+        isTranslator: res.result.legacy.is_translator,
+        translatorType: res.result.legacy.translator_type,
+        hasCustomTimelines: res.result.legacy.has_custom_timelines,
+        createdAt: res.result.legacy.created_at,
+        pinnedTweetIds: res.result.legacy.pinned_tweet_ids_str,
+        isProtected: res.result.legacy.protected,
+    }
+
+    const user: User = {
+        avatar: avatar,
+        banner: banner,
+        profile: profile,
+    }
+
+    return user
+}
