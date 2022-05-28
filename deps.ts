ba2db9f3183c44828a2fd1a319afd8be064f3e31
p1atdev <shu_teiei@outlook.jp>
2022-05-28 23:27:47 +0900

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/twitterql@0.2.1-alpha.1/mod.ts";
+export * from "https://deno.land/x/twitterql@0.2.1-alpha.2/mod.ts";
 export * from "https://deno.land/x/oak@v10.6.0/mod.ts";
 export * from "https://deno.land/std@0.141.0/testing/asserts.ts";
 export * from "https://deno.land/x/sleep@v1.2.1/mod.ts";
